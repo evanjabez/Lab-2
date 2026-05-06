6275372bed22c3dd55143601c9536f9e70402023
Evan <EVANJABEZ.25@ichat.sp.edu.sg>
2026-05-06 09:39:41 +0800

    Added BMI calculation function

--- lab2.py
+++ lab2.py
@@ -1,1 +1,18 @@
-print("I am a C")
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    # Calculate BMI
+    bmi = weight / (height ** 2)
+
+    # Display BMI
+    print("BMI =", bmi)
+
+    if bmi < 18.5:
+        return -1
+    elif bmi < 25:
+        return 0
+    else:
+        return 1
+
+calculate_bmi(weight=57, height=1.73)
